cf2cb52a524713c362df913f079c406e63f317f3
Sreelatha Chunduri <chunduri.sreelatha@gmail.com>
2020-02-01 00:30:32 +0100

    updated mysql

---
+++ labs/module_1/mySQL/update.sql
@@ -0,0 +1,15 @@
+UPDATE `lab_mysql`.`customers`
+SET
+`email` = 'ppicasso@gmail.com'
+WHERE `customer_id` = '10001';
+
+
+UPDATE `lab_mysql`.`customers`
+SET
+`email` = 'lincoln@us.gov'
+WHERE `customer_id` = '20001';
+
+UPDATE `lab_mysql`.`customers`
+SET
+`email` = 'hello@napoleon.me'
+WHERE `customer_id` = '30001';
