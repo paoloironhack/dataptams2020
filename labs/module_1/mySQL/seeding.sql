6d8f0766a7a5fb956f8c8bd0ba60e6eecd5d274d
evadonaque <evadonaque@gmail.com>
2020-02-01 10:19:45 +0100

    changes

---
+++ labs/module_1/mySQL/seeding.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO cars (vin, manufacturer, model, year_model, car_color) VALUES("3K096I98581DHSNUP", "Volkswagen", "Tiguan", 2019, "Blue");
+INSERT INTO customers (customer_id, name_customer, phone_number_customer, email_customer, address_customer, city_customer) VALUES (10001, "Pablo Picasso", "+34 636 17 63 82", "-", "Paseo Chopera 14", "Madrid")
+INSERT INTO salesperson (staff_id, name, store) VALUES ("00001", "Petey Cruiser", "Madrid");
+INSERT INTO invoices (invoice_number, date, vin, customer_id, staff_id) VALUES ("852399038", 2018-08-22, "3K096I98581DHSNUP", "10001", "00004");
