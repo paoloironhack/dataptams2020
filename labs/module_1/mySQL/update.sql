385da918ae06f1853a4218d0d0a40319f71c0922
Maaike de Jong <maaike.de.jong@dans.knaw.nl>
2020-02-03 20:51:47 +0100

    mySQL lab completd

---
+++ labs/module_1/mySQL/update.sql
@@ -0,0 +1,15 @@
+UPDATE salespersons
+SET Store = 'Miami'
+WHERE Store = 'Mimia';
+
+UPDATE customers
+SET Email = 'ppicasso@gmail.com'
+WHERE Name = 'Pablo Picasso';
+
+UPDATE customers
+SET Email = 'lincoln@us.gov'
+WHERE Name = 'Abraham Lincoln';
+
+UPDATE customers
+SET Email = 'hello@napoleon.me'
+WHERE Name = 'Napoléon Bonaparte';
