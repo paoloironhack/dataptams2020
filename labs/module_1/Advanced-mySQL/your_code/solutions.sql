6f8775eabbdc1f29fd25662cd747504720935859
Sreelatha Chunduri <chunduri.sreelatha@gmail.com>
2020-02-07 23:08:43 +0100

    sql advance

--- labs/module_1/Advanced-mySQL/your_code/solutions.sql
+++ labs/module_1/Advanced-mySQL/your_code/solutions.sql
@@ -1,4 +1,4 @@
-SELECT authors.au_id, titleauthor.title_id, sales.qty*titles.royalty, titles.advance/count(authors.au_id) 
+SELECT authors.au_id, titleauthor.title_id, titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty, titles.advance * titleauthor.royaltyper / 100 as advance
 FROM authors
 INNER JOIN titleauthor
 ON authors.au_id = titleauthor.au_id
@@ -7,5 +7,4 @@ ON titleauthor.title_id = titles.title_id
 INNER JOIN roysched
 ON titles.title_id = roysched.title_id
 INNER JOIN sales
-ON titles.title_id = sales.title_id
-GROUP BY titleauthor.title_id
+ON titles.title_id = sales.title_id;
